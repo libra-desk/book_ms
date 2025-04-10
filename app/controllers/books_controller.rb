cfff6c9fa4e4b4ec205d360f892a306980f7cc80
JUDIS <61043700+judis007@users.noreply.github.com>
2025-04-10 11:16:30 +0530

    added functionality for rendering json with pdfs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,44 @@
 class BooksController < ApplicationController
   def index
-    books = Book.all
-    render json: books
+    books = Book.includes(pdf_file_attachment: :blob).all
+
+    render json: books.map { |book|
+      {
+        id: book.id,
+        title: book.title,
+        description: book.description,
+        pages: book.pages,
+        isbn: book.isbn,
+        pdf_file: book.pdf_file.attached? ? url_for(book.pdf_file) : nil
+      }
+    }
   end
 
   def show
     book = Book.find(params[:id])
 
-    render json: book
+    render json: {
+      id: book.id,
+      title: book.title,
+      description: book.description,
+      pages: book.pages,
+      isbn: book.isbn,
+      pdf_file: book.pdf_file.attached? ? url_for(book.pdf_file) : nil
+    }
   end
 
   def create
     book = Book.new(book_params)
 
     if book.save
-      render json: book, status: :created
+      render json: {
+        id: book.id,
+        title: book.title,
+        description: book.description,
+        pages: book.pages,
+        isbn: book.isbn,
+        pdf_file: book.pdf_file.attached? ? url_for(book.pdf_file) : nil
+      }, status: :created
     else
       render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
     end
@@ -24,7 +48,14 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
 
     if book.update(book_params)
-      render json: book
+      render json: {
+        id: book.id,
+        title: book.title,
+        description: book.description,
+        pages: book.pages,
+        isbn: book.isbn,
+        pdf_file: book.pdf_file.attached? ? url_for(book.pdf_file) : nil
+      }
     else
       render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
     end
@@ -35,7 +66,7 @@ class BooksController < ApplicationController
     # if you use find method, then if the book is not there, that error json wont be shown.
     # Instead it will raise an ActiveRecord::RecordNotFound exception. Either you use find_by method
     # or put it inside a rescue block instead of the if else block.
-    book = Book.find(id: params[:id])
+    book = Book.find_by(id: params[:id])
 
     book.destroy
     head :no_content # This is used to send back a success response with no response body
@@ -46,7 +77,8 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:title,
+    params.require(:book)
+          .permit(:title,
                   :description,
                   :pdf_file,
                   :pages,
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_one_attached :pdf_file
   # validates_presence_of :title,
   #                       :description,
   #                       :pages,
