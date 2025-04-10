24b594e3f23caef15d0260720f2c5b510806c526
JUDIS <61043700+judis007@users.noreply.github.com>
2025-04-10 11:51:09 +0530

    added jwt token verification functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+
+  # This basically cross checks the token to verify that it is a genuine request
+  def authenticate_request
+    token = request.headers['Authorization']
+    decoded_value = JsonWebToken.decode(token)
+    @current_user = decoded_value ? decoded_value : nil
+
+    unless @current_user
+      render json: { error: "Unauthorized request" },
+             status: :unauthorized
+    end
+  end
 end
